0551c5234c8088c73546162c43a6fb0e28cf410d
pablitomartinez <pablitoemartinez666@gmail.com>
2023-02-14 09:04:47 -0300

    navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
+import Navbar from "./components/NavBar/NavBar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <h1>Hola</h1>
     </div>
   );
