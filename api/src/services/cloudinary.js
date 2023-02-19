bea1e0bab114194931b6be5ce3eeb5d7b90699eb
Ari Hernandez <mikbaor@gmail.com>
2023-02-18 19:31:02 -0600

    cloudinary borrado imagenes

--- api/src/services/cloudinary.js
+++ api/src/services/cloudinary.js
@@ -15,15 +15,22 @@ async function uploadImage (filePath) { // file path es la direccion desde lo en
     //height:
     //width: 
     //etc puedes agregar otras propiedades a la foto 
-
   })
 }
+
 //dale node y el cambia la ruta de tu archivo para ver el urlimage y 
 //poder usarla con el link que arroja el console.log
 
 //console.log(uploadImage("larutadetuimagen"))
 
+// para borrar imagenes requerimos el publicID en el modelo user se encuentra como imagePublicId este
+//es el que debemos usar para borrarla una vez borrada usamos uploadImage para cargar la nueva
+  async function deleteImage (publicId) {
+  cloudinary.uploader.destroy(publicId)
+}
+
 module.exports = {
   cloudinary,
-  uploadImage
+  uploadImage,
+  deleteImage
 }
