ea50676fc2f62f8969a2416c662f6bb7adb1887a
fraynilson2003 <fraynilson2003@gmail.com>
2023-02-25 13:46:23 -0500

    Formulario crear servicio

--- client/src/redux/actions/serviceActions.js
+++ client/src/redux/actions/serviceActions.js
@@ -20,19 +20,29 @@ export const getService = () => {
 
 export const createService = (input) => {
   return async (dispatch) => {
-    const json = JSON.stringify(input);
-    const customConfig = {
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-    };
-    console.log(input);
-    const result = await axios.post("/user/service", json, customConfig);
-    return dispatch({
-      type: ActionTypes.CREATE_SERVICE,
-      payload: result.data,
-    });
+    try {
+      const json = JSON.stringify(input);
+      const customConfig = {
+        headers: {
+          'Authorization': `eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MiwiZmlyc3ROYW1lIjoiVG9tIiwibGFzdE5hbWUiOiJXaWxzb24iLCJlbWFpbCI6InRvbXdpbHNvbkBleGFtcGxlLmNvbSIsInVzZXIiOiJ0b213aWxzb24iLCJwaG9uZSI6IjEyMzQ1Njc4OTQiLCJyb2xlIjoicHJvZmVzc2lvbmFsIiwiaWF0IjoxNjc3MzUwMTM3LCJleHAiOjE2Nzk5NDIxMzd9.w1Qm7ttqP3Kn98T_B_bSZNkQVX0NhWmhQjelzpDaguI`
+        }
+      }
+      
+      console.log("Esto llega");
+      console.log(input);
+
+      const result = await axios.post("/user/service", input, customConfig);
+
+      return dispatch({
+        type: ActionTypes.CREATE_SERVICE,
+        payload: result.data,
+      });
+    } catch (error) {
+      alert("Relleno correctamente los formularios")
+      throw new Error("Error en createService")
+    }
+
+    
   };
 };
 
