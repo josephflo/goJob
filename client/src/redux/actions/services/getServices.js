d047b289742b2a92a35b720cfda8b387fd7136e2
victor-cp <vcolquichaguap@uni.pe>
2023-02-26 00:40:43 -0500

    correct-action-get-service-victor

--- client/src/redux/actions/services/getServices.js
+++ client/src/redux/actions/services/getServices.js
@@ -3,17 +3,18 @@ import convertObjToQuery from "../../../helpers/convertObjToQuery";
 import { ActionTypes } from "../../constants/actions-types";
 
 export let getAllServices = (objQuery) => async (dispatch) => {
-  let queries = {}
+  let queries = {};
 
-  if(objQuery.page) queries.page = objQuery.page
-  if(objQuery.page_size) queries.page_size = objQuery.page_size
-  if(objQuery.state) queries.state = objQuery.state
-  if(objQuery.orderFecha) queries.orderFecha = objQuery.orderFecha
+  if (objQuery.page) queries.page = objQuery.page;
+  if (objQuery.page_size) queries.page_size = objQuery.page_size;
+  if (objQuery.state) queries.state = objQuery.state;
+  if (objQuery.orderFecha) queries.orderFecha = objQuery.orderFecha;
 
-  if(objQuery.tittle && objQuery.tittle != "") queries.tittle = objQuery.tittle
-  if(objQuery.provincia) queries.provincia = objQuery.provincia
-  if(objQuery.ciudad) queries.ciudad = objQuery.ciudad
-  if(objQuery.job) queries.job = objQuery.job
+  if (objQuery.tittle && objQuery.tittle != "")
+    queries.tittle = objQuery.tittle;
+  if (objQuery.provincia) queries.provincia = objQuery.provincia;
+  if (objQuery.ciudad) queries.ciudad = objQuery.ciudad;
+  if (objQuery.job) queries.job = objQuery.job;
 
   const concatQuery = convertObjToQuery(queries);
 
@@ -24,7 +25,15 @@ export let getAllServices = (objQuery) => async (dispatch) => {
     console.log("Nueva peticion de services");
     console.log(respuesta.result);
 
-    return dispatch({ type: ActionTypes.GET_SERVICE, payload: respuesta });
+    return dispatch({
+      type: ActionTypes.GET_SERVICE,
+      payload: {
+        respuesta: respuesta,
+        nextPage: result.nextPage,
+        previousPage: result.previousPage,
+        totalPages: result.totalPages,
+      },
+    });
   } catch (error) {
     console.log("No trajo services");
     throw Error("mallllllllllllll");
