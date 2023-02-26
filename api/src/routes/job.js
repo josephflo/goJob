2b717f5f3ecc84cad1785035e1572be9f34ceb2d
mikbaor <mikbaor@gmail.com>
2023-02-26 13:00:54 -0600

    api

---
+++ api/src/routes/job.js
@@ -0,0 +1,21 @@
+const { Router} = require('express');
+const jobHandlers = require("../handlers/jobHandler");
+const { auth, verifyRole } = require('../middlewares/auth');
+
+const router = Router();
+/*******GET ********* */
+router.get("/", jobHandlers.getAllJobs);
+router.get("/:id", jobHandlers.getIdJob);
+
+/***** POST ******* */
+router.post("/", jobHandlers.createJob);
+
+/****** DELETE ******** */
+router.delete("/:id", jobHandlers.deleteJob);
+
+/******** PUT ********* */
+router.put("/:id", jobHandlers.actulizarJob);
+
+
+
+module.exports = router;
