417106466b295ddd54b0ad8af8af106904e24a30
fraynilson2003 <fraynilson2003@gmail.com>
2023-03-01 23:16:06 -0500

    nueva data

--- client/src/App.js
+++ client/src/App.js
@@ -51,7 +51,7 @@ import ServicesDashboard from "./pages/AdminDashboard/serviceDashboard";
 
 // Default axios
 //axios.defaults.baseURL = "https://deploy-pi-production-4388.up.railway.app/";
-axios.defaults.baseURL = "https://gojob-production.up.railway.app/";
+axios.defaults.baseURL = "https://gojob2-production.up.railway.app/";
 
 function App() {
   const { isAuthenticated, user, isLoading } = useAuth0();
