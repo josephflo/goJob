242564fe01fa5fe787b26218519ede2aec522386
fraynilson2003 <fraynilson2003@gmail.com>
2023-03-06 15:37:46 -0500

    filtros professionales

--- client/src/pages/jobPage/JobPage.jsx
+++ client/src/pages/jobPage/JobPage.jsx
@@ -5,14 +5,15 @@ import JobCard from "./jobCard/JobCard";
 export default function JobPage() {
   const jobs = useSelector((state) => state.jobs);
 
+  let profesionales = jobs.slice(0,5)
+
   return (
     <div>
       <h1 className=" text-5xl text-black mx-auto pt-20 text-center">
         ¿Qué es lo que buscas?
       </h1>
       <div className="py-24 flex flex-wrap items-center justify-center ">
-        {jobs?.length > 0 ? (
-          jobs?.map((e) => {
+        {profesionales?.length > 0 ? (profesionales?.map((e) => {
             return (
               <JobCard
                 id={e.id}
