2e64ba256cb4cec219b0a5d6a99ef1449f0a423f
victor-cp <vcolquichaguap@uni.pe>
2023-03-08 00:25:53 -0500

    3 opiniones resuelto

---
+++ client/src/components/detailProfessional/components/Opinion3.jsx
@@ -0,0 +1,45 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  getUserDetail,
+  getUserDetailOpinion3,
+} from "../../../redux/actions/userActions";
+
+export default function Opinion3({ detail }) {
+  const dipatch = useDispatch();
+  const userDetailOpinion3 = useSelector((state) => state.userDetailOpinion3);
+
+  useEffect(() => {
+    dipatch(getUserDetailOpinion3(detail.idUser));
+  }, []);
+
+  return (
+    <>
+      <h1 className="text-2xl pb-4 font-semibold">
+        Opiniones de nuestros usuarios
+      </h1>
+      <ul className="">
+        <>
+          <li className=" flex grid grid-cols-5 p-2">
+            <div className="col-span-1">
+              <img
+                src={userDetailOpinion3.imagePerfil}
+                className=" h-30 rounded-full"
+                alt=""
+              />
+            </div>
+            <div className="col-span-3">
+              <p className="text-2xl font-medium">
+                {userDetailOpinion3.firstName}
+              </p>
+              <p className="text-base font-medium text-gray-500 ">
+                {detail.date}
+              </p>
+              <p className=" pt-2 font-medium">{detail.review}</p>
+            </div>
+          </li>
+        </>
+      </ul>
+    </>
+  );
+}
