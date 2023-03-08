0935cc3963523b2bb712856ff304561b93452942
mikbaor <mikbaor@gmail.com>
2023-03-07 18:58:42 -0600

    user detail

--- client/src/pages/AdminDashboard/formUser.jsx
+++ client/src/pages/AdminDashboard/formUser.jsx
@@ -1,30 +1,20 @@
 import React from "react";
 import { useSelector } from "react-redux";
 
-
-
 // components
 
 export default function FormUser() {
-
-  const detailUser = useSelector((state)=> state.userDetail)
-
-  //   const dispatch = useDispatch()
-  
-  // useEffect(()=>{
-  //   dispatch(getUserDetail(1))
-  // },[])
-   
-  
-  console.log({modifyuser:detailUser})
+  const detailUser = useSelector((state) => state.userDetail);
 
   return (
     <>
       <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blue-100 border-0">
         <div className="rounded-t bg-white mb-0 px-6 py-6">
           <div className="text-center flex justify-between">
-            <h6 className="text-gray-700 text-xl font-bold">Detalles del Usuario</h6>
-            
+            <h6 className="text-gray-700 text-xl font-bold">
+              Detalles del Usuario
+            </h6>
+
             <button
               className="bg-blue-600 text-white active:bg-blue-900 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150"
               type="button"
@@ -51,6 +41,7 @@ export default function FormUser() {
                     type="text"
                     className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
                     placeholder="Username"
+                    defaultValue={detailUser.user}
                   />
                 </div>
               </div>
@@ -65,7 +56,7 @@ export default function FormUser() {
                   <input
                     type="email"
                     className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                    placeholder="email"
+                    defaultValue={detailUser.email}
                   />
                 </div>
               </div>
@@ -81,6 +72,7 @@ export default function FormUser() {
                     type="text"
                     className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
                     placeholder="Nombres"
+                    defaultValue={detailUser.firstName}
                   />
                 </div>
               </div>
@@ -96,6 +88,7 @@ export default function FormUser() {
                     type="text"
                     className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
                     placeholder="Apellidos"
+                    defaultValue={detailUser.lastName}
                   />
                 </div>
               </div>
@@ -115,11 +108,19 @@ export default function FormUser() {
                   >
                     Calle
                   </label>
-                  <input
-                    type="text"
-                    className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                    placeholder="Detalle de Calle"
-                  />
+                  {detailUser.direccion ? (
+                    <input
+                      type="text"
+                      className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                      defaultValue={detailUser.direccion}
+                    />
+                  ) : (
+                    <input
+                      type="text"
+                      className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                      placeholder="N/E"
+                    />
+                  )}
                 </div>
               </div>
               <div className="w-full lg:w-4/12 px-4">
@@ -130,11 +131,19 @@ export default function FormUser() {
                   >
                     Ciudad
                   </label>
-                  <input
-                    type="email"
-                    className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                    placeholder="Ciudad"
-                  />
+                  {detailUser.provincia ? (
+                    <input
+                      type="email"
+                      className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                      defaultValue={detailUser.provincia}
+                    />
+                  ) : (
+                    <input
+                      type="email"
+                      className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                      placeholder="N/E"
+                    />
+                  )}
                 </div>
               </div>
               <div className="w-full lg:w-4/12 px-4">
@@ -145,11 +154,19 @@ export default function FormUser() {
                   >
                     Pais
                   </label>
-                  <input
-                    type="text"
-                    className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                    placeholder="Pais"
-                  />
+                  {detailUser.country ? (
+                    <input
+                      type="text"
+                      className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                      defaultValue={detailUser.country}
+                    />
+                  ) : (
+                    <input
+                      type="text"
+                      className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                      placeholder="N/E"
+                    />
+                  )}
                 </div>
               </div>
               <div className="w-full lg:w-4/12 px-4">
@@ -160,20 +177,42 @@ export default function FormUser() {
                   >
                     Codigo Postal
                   </label>
-                  <input
-                    type="text"
-                    className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                    placeholder="Codigo Postal"
-                  />
+                  {detailUser.cp ? (
+                    <input
+                      type="text"
+                      className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                      defaultValue={detailUser.cp}
+                    />
+                  ) : (
+                    <input
+                      type="text"
+                      className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                      placeholder="N/E"
+                    />
+                  )}
                 </div>
               </div>
             </div>
 
             <hr className="mt-6 border-b-1 border-blueGray-300" />
+            {detailUser.Jobs?.length ? (
+              <h6 className="text-gray-400 text-sm mt-3 mb-6 font-bold uppercase">
+                JOBS
+                {detailUser.Jobs?.map((job, index) => (
+                  <span
+                    key={index}
+                    className="text-xs py-1 px-2 bg-blue-200 font-bold text-blue-600 ml-2"
+                  >
+                    {job.name}
+                  </span>
+                ))}
+              </h6>
+            ) : (
+              <h6 className="text-gray-400 text-sm mt-3 mb-6 font-bold uppercase">
+                SIN JOBS
+              </h6>
+            )}
 
-            <h6 className="text-gray-400 text-sm mt-3 mb-6 font-bold uppercase">
-              JOBS
-            </h6>
             <div className="flex flex-wrap">
               <div className="w-full lg:w-12/12 px-4">
                 <div className="relative w-full mb-3">
@@ -183,14 +222,23 @@ export default function FormUser() {
                   >
                     Descripcion
                   </label>
-                  <textarea
-                    type="text"
-                    className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                    rows="4"
-                    placeholder="Descripcion"
-                  ></textarea>
+                  {detailUser.description ? (
+                    <textarea
+                      type="text"
+                      className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                      rows="4"
+                      defaultValue={detailUser.description}
+                    ></textarea>
+                  ) : (
+                    <textarea
+                      type="text"
+                      className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                      rows="4"
+                      placeholder="N/E"
+                    ></textarea>
+                  )}
                 </div>
-                </div>
+              </div>
             </div>
           </form>
         </div>
--- client/src/pages/AdminDashboard/profile.jsx
+++ client/src/pages/AdminDashboard/profile.jsx
@@ -8,7 +8,7 @@ import Swal from "sweetalert2";
 export default function Profile() {
   const detailUser = useSelector((state)=> state.userDetail)
 
-
+console.log(detailUser.imagePerfil)
   return (
     <>
    
@@ -16,21 +16,22 @@ export default function Profile() {
         <div className="px-6">
        
           <div className="flex flex-wrap justify-center">
-            <div className="w-full px-4 flex justify-center">
-              <div className="relative">
+            <div className="w-full px-4 r">
+              <div className="relative flex justify-center items-center">
                 
                 <img
                   alt=""
                   src={detailUser.imagePerfil}
-                 className="shadow-xl rounded-full h-auto items-center border-none relative -m-16 -ml-20 lg:-ml-16 max-w-150-px"
+                 className="shadow-xl rounded-full h-auto items-center border-none relative -m-20 -ml-25 lg:-ml-30 max-w-200-px "
                 />
               </div>
             </div>
             <div className="w-full px-4 text-center mt-20">
               <div className="flex justify-center py-4 lg:pt-4 pt-8">
                 <div className="mr-4 p-3 text-center">
+                  {}
                   <span className="text-xl font-bold block uppercase tracking-wide text-blueGray-600">
-                    {detailUser.myTrabajos.length}
+                    {detailUser.myTrabajos?.length}
                   </span>
                   <span className="text-sm text-gray-400">Servicios</span>
                 </div>
@@ -48,17 +49,23 @@ export default function Profile() {
             <h3 className="text-xl font-semibold leading-normal mb-2 text-blueGray-700 mb-2">
              { detailUser.firstName}
             </h3>
+            {detailUser.provincia?
             <div className="text-sm leading-normal mt-0 mb-2 text-gray-400 font-bold uppercase">
-              <i className="fas fa-map-marker-alt mr-2 text-lg text-gray-400"></i>{" "}
+            <i className="fas fa-map-marker-alt mr-2 text-lg text-gray-400"></i>{" "}
               {detailUser.provincia}
-            </div>
+            </div>:
+            <div className="text-sm leading-normal mt-0 mb-2 text-gray-400 font-bold uppercase">
+            <i className="fas fa-map-marker-alt mr-2 text-lg text-gray-400"></i>{" "}
+             Sin registro
+            </div>}
+            
             <div className="mb-2 text-gray-600 mt-10">
               <i className="fas fa-briefcase mr-2 text-lg text-gray-400"></i>
               {detailUser.user}
             </div>
             <div className="mb-2 text-gray-600">
               <i className="fas fa-university mr-2 text-lg text-gray-400"></i>
-              {"id "+ detailUser.id}
+              {"ID: "+ detailUser.id}
             </div>
           </div>
           <div className="mt-10 py-10 border-t border-blueGray-200 text-center">
@@ -84,10 +91,7 @@ export default function Profile() {
               onClick={""} 
               >
                 Activar
-              </button>
-              
-               
-               
+              </button>              
               }
             
              
