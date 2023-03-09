31751b21e10013d47ea2d41b1e0a58c7c1ec87b2
fraynilson2003 <fraynilson2003@gmail.com>
2023-03-08 23:38:30 -0500

    a

--- client/src/containers/filters/PaginationServices.jsx
+++ client/src/containers/filters/PaginationServices.jsx
@@ -56,6 +56,10 @@ const PaginationServices = () => {
     }
   }, [service])
 
+  useEffect(()=>{
+    changePage(1)
+  }, [])
+
 
   let clasePage =  "cursor-pointer px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
 
